7be94ad26a8ee9bd96e2e7063b1513e4dbc64cbe
maxskaink <mangelcvivas@unicauca.edu.co>
2025-04-15 22:09:28 -0500

    Basic creatiion of the core service and the postgres server with docker

---
+++ CoreService/src/test/java/unicauca/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package unicauca.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
