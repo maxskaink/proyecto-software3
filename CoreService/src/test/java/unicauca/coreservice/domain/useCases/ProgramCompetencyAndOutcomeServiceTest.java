ef8fc81364126f59c7c56b63fb6584fd6d73922c
maxskaink <mangelcvivas@unicauca.edu.co>
2025-05-21 14:59:23 -0500

    Some renames, and optimization for tests

--- CoreService/src/test/java/unicauca/coreservice/domain/useCases/ProgramCompetencyAndOutcomeServiceTest.java
+++ CoreService/src/test/java/unicauca/coreservice/domain/useCases/ProgramCompetencyAndOutcomeServiceTest.java
@@ -1,9 +1,6 @@
 package unicauca.coreservice.domain.useCases;
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -12,17 +9,14 @@ import unicauca.coreservice.application.out.ProgramCompetencyAndOutcomeRepositor
 import unicauca.coreservice.domain.exception.DuplicateInformation;
 import unicauca.coreservice.domain.exception.InvalidValue;
 import unicauca.coreservice.domain.exception.NotFound;
-import unicauca.coreservice.domain.model.OptionalWrapper;
 import unicauca.coreservice.domain.model.ProgramCompetency;
 import unicauca.coreservice.domain.model.ProgramOutcome;
-import unicauca.coreservice.domain.model.SubjectCompetency;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 @Transactional
 @SpringBootTest
@@ -66,9 +60,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
 
         assertNotNull(firstInsert, "first competency must be not null");
 
-        Exception exception = assertThrows(DuplicateInformation.class, () -> {
-            service.add(competency);
-        });
+        assertThrows(DuplicateInformation.class, () -> service.add(competency));
     }
 
     @Test
@@ -77,15 +69,11 @@ class ProgramCompetencyAndOutcomeServiceTest {
         ProgramOutcome outcome = new ProgramOutcome(1, "Test Outcome");
         
         // Act & Assert - Missing description
-        Exception exception1 = assertThrows(InvalidValue.class, () -> {
-            new ProgramCompetency(null, null, "Basic", outcome, new ArrayList<>());
-        });
+        Exception exception1 = assertThrows(InvalidValue.class, () -> new ProgramCompetency(null, null, "Basic", outcome, new ArrayList<>()));
         assertEquals("The description can not be null or empty", exception1.getMessage());
         
         // Act & Assert - Missing level
-        Exception exception2 = assertThrows(InvalidValue.class, () -> {
-            new ProgramCompetency(null, "Test Competency", null, outcome, new ArrayList<>());
-        });
+        Exception exception2 = assertThrows(InvalidValue.class, () -> new ProgramCompetency(null, "Test Competency", null, outcome, new ArrayList<>()));
         assertEquals("The competency level can not be null or empty", exception2.getMessage());
     }
 
@@ -104,7 +92,6 @@ class ProgramCompetencyAndOutcomeServiceTest {
             service.add(cmp);
 
         List<ProgramCompetency> result = service.listAllProgramCompetencies();
-        System.out.println("por que esto da null? "+expectedCompetencies.get(1).getId());
         // Assert
         assertEquals(expectedCompetencies.size(), result.size());
         assertEquals(expectedCompetencies.get(0).getDescription(), result.get(0).getDescription());
@@ -128,23 +115,19 @@ class ProgramCompetencyAndOutcomeServiceTest {
     }
 
     @Test
-    void getProgramCompetencyById_shouldThrowExceptionWhenNotExists() throws Exception {
+    void getProgramCompetencyById_shouldThrowExceptionWhenNotExists() {
         // Arrange
         Integer id = 999;
 
         // Act & Assert
-        Exception exception = assertThrows(NotFound.class, () -> {
-            service.getProgramCompetencyById(id);
-        });
+        assertThrows(NotFound.class, () -> service.getProgramCompetencyById(id));
         
     }
 
     @Test
     void getProgramCompetencyById_shouldThrowExceptionWhenIdIsNull() {
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.getProgramCompetencyById(null);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.getProgramCompetencyById(null));
         
         assertEquals("The id is not valid, it can not be null", exception.getMessage());
     }
@@ -172,9 +155,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
         ProgramCompetency competency = new ProgramCompetency(1, "Test Competency", "Basic", outcome, new ArrayList<>());
         
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.updateProgramCompetency(null, competency);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.updateProgramCompetency(null, competency));
         
         assertEquals("The id is not valid, it can not be null", exception.getMessage());
     }
@@ -182,14 +163,9 @@ class ProgramCompetencyAndOutcomeServiceTest {
     @Test
     void updateProgramCompetency_shouldThrowExceptionWhenCompetencyIsNull() {
         //Arrange
-
-        ProgramOutcome newOutcome = new ProgramOutcome(1,"algo");
-
         // Act & Assert
 
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.updateProgramCompetency(1, null);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.updateProgramCompetency(1, null));
         
         assertEquals("Instance of competency is invalid, it can not be null", exception.getMessage());
     }
@@ -204,9 +180,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
         ProgramCompetency compAdded= service.add(expectedCompetency);
         ProgramCompetency result = service.remove(compAdded.getId());
 
-        assertThrows(NotFound.class, () -> {
-            service.getProgramCompetencyById(compAdded.getId());
-        });
+        assertThrows(NotFound.class, () -> service.getProgramCompetencyById(compAdded.getId()));
 
         // Assert
         assertNotNull(result);
@@ -216,9 +190,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
     @Test
     void remove_shouldThrowExceptionWhenIdIsNull() {
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.remove(null);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.remove(null));
         
         assertEquals("The id is not valid, it can not be null", exception.getMessage());
     }
@@ -232,18 +204,18 @@ class ProgramCompetencyAndOutcomeServiceTest {
                 new ProgramOutcome(2, "Outcome 2")
         );
 
-        List<ProgramCompetency> exptectedCompetency = Arrays.asList(
+        List<ProgramCompetency> expectedCompetency = Arrays.asList(
             new ProgramCompetency(1, "Test Competency", "Basic", expectedOutcomes.get(0), new ArrayList<>()),
             new ProgramCompetency(1, "Test Competency 2", "Basic", expectedOutcomes.get(1), new ArrayList<>())
         );
         // Act
-        for(ProgramCompetency cmp: exptectedCompetency)
+        for(ProgramCompetency cmp: expectedCompetency)
             service.add(cmp);
 
         List<ProgramOutcome> result = service.listAllProgramOutcomes();
 
         // Assert
-        assertEquals(exptectedCompetency.size(), result.size());
+        assertEquals(expectedCompetency.size(), result.size());
     }
 
     @Test
@@ -262,23 +234,17 @@ class ProgramCompetencyAndOutcomeServiceTest {
     }
 
     @Test
-    void getProgramOutcomeById_shouldThrowExceptionWhenNotExists() throws Exception {
+    void getProgramOutcomeById_shouldThrowExceptionWhenNotExists(){
         // Arrange
         Integer id = 999;
-        Exception expectedException = new Exception("Outcome not found");
-        
         // Act & Assert
-        Exception exception = assertThrows(Exception.class, () -> {
-            service.getProgramOutcomeById(id);
-        });
+        assertThrows(Exception.class, () -> service.getProgramOutcomeById(id));
     }
 
     @Test
     void getProgramOutcomeById_shouldThrowExceptionWhenIdIsNull() {
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.getProgramOutcomeById(null);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.getProgramOutcomeById(null));
         
         assertEquals("The id is not valid, it can not be null", exception.getMessage());
     }
@@ -290,7 +256,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
         ProgramCompetency competency = new ProgramCompetency(1, "Test Competency", "Basic", outcome, new ArrayList<>());
 
         // Act
-        ProgramCompetency compAdded = service.add(competency);
+        service.add(competency);
         List<ProgramOutcome> outcomes = service.listAllProgramOutcomes();
         ProgramOutcome firstOutcome = outcomes.getFirst();
         ProgramOutcome result = service.updateProgramOutcome(firstOutcome.getId(), outcome);
@@ -307,9 +273,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
         ProgramOutcome outcome = new ProgramOutcome(1, "Test Outcome");
         
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.updateProgramOutcome(null, outcome);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.updateProgramOutcome(null, outcome));
         
         assertEquals("The id is not valid, it can not be null", exception.getMessage());
     }
@@ -317,9 +281,7 @@ class ProgramCompetencyAndOutcomeServiceTest {
     @Test
     void updateProgramOutcome_shouldThrowExceptionWhenOutcomeIsNull() {
         // Act & Assert
-        Exception exception = assertThrows(InvalidValue.class, () -> {
-            service.updateProgramOutcome(1, null);
-        });
+        Exception exception = assertThrows(InvalidValue.class, () -> service.updateProgramOutcome(1, null));
         
         assertEquals("Instance of outcome is invalid, it can not be null", exception.getMessage());
     }
