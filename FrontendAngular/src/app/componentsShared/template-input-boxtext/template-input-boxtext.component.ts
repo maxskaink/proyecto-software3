d8591b6b2b81e879b03817c724f17939f4660352
Santiago <santiescandon1@gmail.com>
2025-05-25 02:09:40 -0500

    add message of campus neccesary in input

--- FrontendAngular/src/app/componentsShared/template-input-boxtext/template-input-boxtext.component.ts
+++ FrontendAngular/src/app/componentsShared/template-input-boxtext/template-input-boxtext.component.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-template-input-boxtext',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './template-input-boxtext.component.html',
   styleUrl: './template-input-boxtext.component.css'
 })
 export class TemplateInputBoxtextComponent {
   @Input() description: string = '';
+  inputValue: string= '';
+  touched: boolean = false;
+  onBlur() {
+    this.touched= true;
+  }
 }
+
