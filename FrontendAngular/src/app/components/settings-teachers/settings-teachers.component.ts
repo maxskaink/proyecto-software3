a5fdd86f6e4e1f187fb9726861d8d442feb673b7
Santiago <santiescandon1@gmail.com>
2025-05-25 23:10:12 -0500

    add list teacher

--- FrontendAngular/src/app/components/settings-teachers/settings-teachers.component.ts
+++ FrontendAngular/src/app/components/settings-teachers/settings-teachers.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MoleculeBackHeaderComponent } from '../../componentsShared/molecule-back-header/molecule-back-header.component';
+import { TemplateListTeachersComponent } from '../../componentsShared/template-list-teachers/template-list-teachers.component';
 
 @Component({
   selector: 'app-settings-teachers',
-  imports: [CommonModule, MoleculeBackHeaderComponent],
+  imports: [CommonModule, MoleculeBackHeaderComponent, TemplateListTeachersComponent],
   templateUrl: './settings-teachers.component.html',
   styleUrl: './settings-teachers.component.css'
 })
