f891acfef54009d812fc6c4ffb95fc2f43d132d2
Santiago <santiescandon1@gmail.com>
2025-05-26 22:03:04 -0500

    modificade template header title

---
+++ FrontendAngular/src/app/componentsShared/template-header-title/template-header-title.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MoleculeBackHeaderComponent } from '../molecule-back-header/molecule-back-header.component';
+
+@Component({
+  selector: 'app-template-header-title',
+  imports: [CommonModule, MoleculeBackHeaderComponent],
+  templateUrl: './template-header-title.component.html',
+  styleUrl: './template-header-title.component.css'
+})
+export class TemplateHeaderTitleComponent {
+  @Input() title: string = '';
+  @Input() description: string ='';
+}
