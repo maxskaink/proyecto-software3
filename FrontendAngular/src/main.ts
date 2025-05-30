10b73c8f197085b1dfe9adc4c8c807719c109481
maxskaink <mangelcvivas@unicauca.edu.co>
2025-05-29 20:28:52 -0500

    Fix a warning and delte inecesary imports

--- FrontendAngular/src/main.ts
+++ FrontendAngular/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors,withFetch } from '@angular/common/http';
 import { authInterceptor } from './app/interceptors/auth.interceptor';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app/app.routes';
@@ -26,7 +26,7 @@ bootstrapApplication(AppComponent, {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor]),withFetch()),
     provideRouter(routes, withComponentInputBinding()),
     appConfigProvider
   ]
