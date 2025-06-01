b20ad8c82f0349af29c9c99ae398c784542c707a
Santiago <santiescandon1@gmail.com>
2025-06-01 18:59:47 -0500

    add basic create criterion

--- FrontendAngular/src/app/components/create-rubric/create-rubric.component.ts
+++ FrontendAngular/src/app/components/create-rubric/create-rubric.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TemplateHeaderTitleComponent } from '../../componentsShared/templates/template-header-title/template-header-title.component';
+import { TemplateRubricCreateCriterionComponent } from '../../componentsShared/templates/template-rubric-create-criterion/template-rubric-create-criterion.component';
 
 @Component({
   selector: 'app-create-rubric',
-  imports: [CommonModule, TemplateHeaderTitleComponent],
+  imports: [CommonModule, TemplateHeaderTitleComponent, TemplateRubricCreateCriterionComponent],
   templateUrl: './create-rubric.component.html',
   styleUrl: './create-rubric.component.css'
 })
